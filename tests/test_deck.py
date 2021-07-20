27ff42bd75b048dab20c143cffff864b9172ef05
Business Data Wizard <jowi.legal.ia@gmail.com>
2021-07-19 22:04:42 -0500

    renamed test_main to test_deck

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -1,7 +1,8 @@
-import numpy as np
-from src.Deck import Deck
-import pytest
 from collections import Counter
+
+import pytest
+
+from src.Deck import Deck
 
 
 @pytest.fixture
@@ -15,7 +16,6 @@ def Deck_52_Counter():
 @pytest.fixture
 def Deck_52_List():
     return 4 * ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
-
 
 def test_deck_has_standard_cards(Deck_52, Deck_52_Counter):
     deck_cards_set = Counter(Deck_52.cards)
@@ -32,7 +32,6 @@ def test_draw_from_top(Deck_52):
     top_card = Deck_52.cards[-1]
     drawn_card = Deck_52.draw_from_top()
     assert  top_card == drawn_card
-
 
 def test_reset_deck(Deck_52, Deck_52_List):
     Deck_52.reset_deck()
