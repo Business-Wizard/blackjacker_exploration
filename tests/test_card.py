b191a1ab5e86732c30333a496ae94db2c352776b
Business Data Wizard <jowi.legal.ia@gmail.com>
2021-07-19 22:35:42 -0500

    repr of Card class

---
+++ tests/test_card.py
@@ -0,0 +1,13 @@
+import pytest
+from src.Card import Card
+
+
+@pytest.fixture
+def Card_Ace_Spades():
+    return Card("Ace", "Spades")
+
+
+def test_card_print(Card_Ace_Spades):
+    printed = Card_Ace_Spades.__repr__()
+    expected = "Ace of Spades"
+    assert printed == expected
